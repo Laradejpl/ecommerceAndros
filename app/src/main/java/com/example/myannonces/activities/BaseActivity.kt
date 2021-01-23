0df068d7fcabe872a2f4358b37d6761ba50d16b4
laradejpl <51596185+Laradejpl@users.noreply.github.com>
2021-01-23 21:08:51 +0100

    validation du register

---
+++ app/src/main/java/com/example/myannonces/activities/BaseActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myannonces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.myannonces.R
+import com.google.android.material.snackbar.Snackbar
+
+open class BaseActivity : AppCompatActivity() {
+
+    // TODO Step 4: Create a function to show the success and error messages in snack bar component.
+    // START
+    /**
+     * A function to show the success and error messages in snack bar component.
+     */
+    fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+        val snackBar =
+                Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (errorMessage) {
+            snackBarView.setBackgroundColor(
+                    ContextCompat.getColor(
+                            this@BaseActivity,
+                            R.color.colorSnackBarError
+                    )
+            )
+        }else{
+            snackBarView.setBackgroundColor(
+                    ContextCompat.getColor(
+                            this@BaseActivity,
+                            R.color.colorSnackBarSuccess
+                    )
+            )
+        }
+        snackBar.show()
+    }
+    // END
+}
--- app/src/main/java/com/example/myannonces/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/myannonces/activities/RegisterActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myannonces.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
 import com.example.myannonces.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_login.et_email as et_email1
+import kotlinx.android.synthetic.main.activity_login.et_password as et_password1
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -14,8 +19,68 @@ class RegisterActivity : AppCompatActivity() {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+
         setContentView(R.layout.activity_register);
 
 
+
+
+        tv_login.setOnClickListener {
+
+            // Launch the register screen when the user clicks on the text.
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_register.setOnClickListener {
+
+            validateRegisterDetails()
+        }
+       
     }
+
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(et_first_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_last_name.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_email.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_confirm_password.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_confirm_password), true)
+                false
+            }
+
+            et_password.text.toString().trim { it <= ' ' } != et_confirm_password.text.toString()
+                    .trim { it <= ' ' } -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_and_confirm_password_mismatch), true)
+                false
+            }
+            !cb_terms_and_condition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar(resources.getString(R.string.registery_successful), false)
+                true
+            }
+        }
+    }
+
+
 }
