87a7918544dc56d9cbb2242127f33abebd48fb23
laradejpl <51596185+Laradejpl@users.noreply.github.com>
2021-01-25 10:35:42 +0100

    ajout de la class USER ,et possibilité en cas de login successfull de passer a la mainActivity

--- app/src/main/java/com/example/myannonces/activities/MainActivity.kt
+++ app/src/main/java/com/example/myannonces/activities/MainActivity.kt
@@ -6,36 +6,9 @@ import android.os.Parcel
 import android.os.Parcelable
 import com.example.myannonces.R
 
-class MainActivity() : AppCompatActivity(), Parcelable {
-    constructor(parcel: Parcel) : this() {
-    }
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
-
-
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MainActivity> {
-        override fun createFromParcel(parcel: Parcel): MainActivity {
-            return MainActivity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MainActivity?> {
-            return arrayOfNulls(size)
-        }
     }
 }
