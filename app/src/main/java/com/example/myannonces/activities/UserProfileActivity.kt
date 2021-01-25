37b92384c33fd28436b3c647e6129e95aa330434
laradejpl <51596185+Laradejpl@users.noreply.github.com>
2021-01-25 14:20:29 +0100

    activite user_profil en plein ecran

--- app/src/main/java/com/example/myannonces/activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/myannonces/activities/UserProfileActivity.kt
@@ -2,12 +2,22 @@ package com.example.myannonces.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.myannonces.R
 
 class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+
+        // mettre en plein ecran
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_user_profile);
+
 
 
 
